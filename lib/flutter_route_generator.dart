29a445a2b7182e8857a8c70e0d4cb92093023721
adddmdr <adddmader@gmail.com>
2025-05-19 10:10:52 +0200

    feat: added dynamic name types for args and cleanup

--- lib/flutter_route_generator.dart
+++ lib/flutter_route_generator.dart
@@ -2,6 +2,4 @@ library flutter_route_generator;
 
 export 'src/models/screen_config.dart';
 export 'src/models/route_config.dart';
-// export 'src/generators/route_generator.dart';
 export 'src/builders/route_builder.dart';
-// export 'routes.dart';
